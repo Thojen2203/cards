1dbbfe1413db5980344bde64f5b67c24ede66c20
Thomas Jenvrin <thomas.jenvrin@etu.unicaen.fr>
2025-10-09 22:15:37 +0200

    exemple de page magasin

--- shop.php
+++ shop.php
@@ -1,41 +1,57 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <?php require_once 'php/head.php'; ?>
-    <title>Document</title>
+  <?php require_once 'php/head.php'; ?>
+  <title>Magasin</title>
 </head>
+
 <body>
-    <?php require_once 'php/header.php'; ?>
-    <?php require_once 'php/navbar.php'; ?>
-    <h1 class="text-center my-4">Bienvenue dans la boutique Kards !</h1>
-    <div>
-    <div class="card-group">
-  <div class="card">
-    <img src="" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+  <?php require_once 'php/header.php'; ?>
+  <?php require_once 'php/navbar.php'; ?>
+  <h1 class="text-center my-4">Bienvenue dans la boutique Kards !</h1>
+  <div>
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card m-3">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Giga pack</h5>
+            <p class="card-text">Ce pack contient 6 cartes, dont 3 communes, 1 inhabituelle à mythique, une rare ou mieux et 1 épique ou mieux.</p>
+            <button type="button" class="btn btn-primary">Acheter</button>
+          </div>
+        </div>
+        <div class="card m-3">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Gros pack</h5>
+            <p class="card-text">Ce pack contient 4 cartes, dont 2 cartes communes, 1 carte commune à rare et 1 rare ou
+              mieux.</p>
+            <button type="button" class="btn btn-primary">Acheter</button>
+          </div>
+        </div>
+        <div class="card m-3">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Pack Plus</h5>
+            <p class="card-text">Ce pack vous donne 1 carte commune, une commune ou inhabituelle et une carte de rareté
+              Inhabituelle ou supérieur.</p>
+            <button type="button" class="btn btn-primary">Acheter</button>
+          </div>
+        </div>
+        <div class="card m-3">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Small Booster</h5>
+            <p class="card-text">Le plus petit des packs ! Un petit pack pour obtenir 1 carte commune et une 2e carte de
+              rareté aléatoire.</p>
+            <button type="button" class="btn btn-primary">Acheter</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-  <div class="card">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-    </div>
-  </div>
-</div>
-    </div>
-    <?php require_once 'php/footer.php'; ?>
+  <?php require_once 'php/footer.php'; ?>
 </body>
+
 </html>
